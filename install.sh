097a83552f152d53804ed1574735784c39b0bd32
Joshua G. Taylor <sonicbphuct@gmail.com>
2017-03-29 19:48:46 +0200

    nvm env

--- install.sh
+++ install.sh
@@ -21,5 +21,7 @@ function wirelessFix() {
 
 function installNVM() {
 	wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 	nvm install v7.7.4
 }
